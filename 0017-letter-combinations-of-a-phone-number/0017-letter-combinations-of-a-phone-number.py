53e1aad282c782d7863530900895c67bd53a4a65
Jyothsna <karuparthi.jyothsna@gmail.com>
2025-11-30 14:40:25 -0500

    Time: 0 ms (100%), Space: 18 MB (22.13%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,18 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        mapping={
+            '2':'abc','3':'def','4':'ghi','5':'jkl',
+            '6':'mno','7':'pqrs','8':'tuv','9':'wxyz'
+        }
+        res=[]
+        def dfs(idx,path):
+            if idx == len(digits):
+                res.append(path)
+                return
+            for ch in mapping[digits[idx]]:
+                dfs(idx+1, path + ch)
+        dfs(0,"")
+        return res
