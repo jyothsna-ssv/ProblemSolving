0cd8463cfd576fd92f3831faac19281695e02b91
Jyothsna <karuparthi.jyothsna@gmail.com>
2025-12-06 18:35:12 -0500

    Time: 0 ms (100%), Space: 17.7 MB (79.49%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -0,0 +1,15 @@
+# sums the last three numbs
+# 0,1,1 and iteratively update them until reaching n.
+# return the last value
+
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0: 
+            return 0
+        if n==1 or n==2: return 1
+
+        t0,t1,t2 = 0,1,1
+        for _ in range(3, n+1):
+            t0,t1,t2 = t1,t2,t0+t1+t2
+        return t2
