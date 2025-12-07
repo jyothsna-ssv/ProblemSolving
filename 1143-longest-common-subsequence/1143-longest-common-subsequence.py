0d79e53c27e9fb8d3daf44a032599cd04a7a1f05
Jyothsna <karuparthi.jyothsna@gmail.com>
2025-12-06 19:01:53 -0500

    Time: 467 ms (58.66%), Space: 42.7 MB (72.8%) - LeetHub

---
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -0,0 +1,18 @@
+# dp[i][j] stores the LCS length of prefixes text1[:i] and text2[:j]
+# match -> add 1 from diagonal
+# no match -> take max of top or left
+# dp[m][n] is the LCS length
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m,n = len(text1), len(text2)
+
+        dp=[[0]* (n+1) for _ in range(m+1)]
+
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if text1[i-1] == text2[j-1]:
+                    dp[i][j] = 1+ dp[i-1][j-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+        return dp[m][n]
